1ea79d0236037d5a0558dc90b6fb8d84b006ba1e
shimasakisan <shimasakisan@gmail.com>
2023-04-30 23:40:58 +0100

    Top message now has several options

--- frontend/src/Intro.js
+++ frontend/src/Intro.js
@@ -1,11 +1,28 @@
+import { useEffect, useState } from 'react';
 import './Intro.css';
 
+const messages = [
+    <><span className="Main">Experience</span> the power of llama.cpp.</>,
+    <><span className="Main">Unleash</span> the potential of LLMs on the edge.</>,
+    <><span className="Main">Revolutionize</span> your chat experience with llama.cpp.</>,
+    <><span className="Main">Join</span> the llama.cpp revolution.</>,
+    <><span className="Main">Empower</span> your chat with llama.cpp.</>,
+]
+
+
 export default function Intro() {
+
+    const [selectedItem, setSelectedItem] = useState(() => {
+        return messages[Math.floor(Math.random() * messages.length)];
+    });
+
+    useEffect(() => {
+        setSelectedItem(messages[Math.floor(Math.random() * messages.length)]);
+    }, []);
+
     return (
         <div className="Intro">
-            <div className="Header">
-                <span className="Main">This</span> is the intro to the thing.
-            </div>
+            <div className="Header">{selectedItem}</div>
         </div>
     )
 }
