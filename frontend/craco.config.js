a751c34164557764916f4854d05bdb7e11bfccfb
shimasakisan <shimasakisan@gmail.com>
2023-05-21 19:35:34 +0100

    frontend: override the default inject policy inject the JS in the body instead of head.
    This fixes the "createRoot(...): Target container is not a DOM element." error on initial load of the index.html file in the browser.

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,15 +1,25 @@
 const InlineChunkHtmlPlugin = require('react-dev-utils/InlineChunkHtmlPlugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// This file configures craco to override defaults in
+// create-react-app's webpack.config.js without ejecting.
 
 module.exports = {
   webpack: {
     configure: (webpackConfig, { env, paths }) => {
 
       webpackConfig.plugins.forEach(plugin => {
+        // Include all JS files inside the HTML, not just the webpack runtime.
         if (plugin instanceof InlineChunkHtmlPlugin) {
-          plugin.tests =  [ /.+[.]js/ ]
+          plugin.tests = [/.+[.]js/]
+        }
+        // Inject JS in the body instead of head
+        if (plugin instanceof HtmlWebpackPlugin) {
+          plugin.userOptions.inject = 'body'
         }
       })
 
+      // Pack the CSS inside index.html
       const oneOfRuleIdx = webpackConfig.module.rules.findIndex(rule => !!rule.oneOf);
       webpackConfig.module.rules[oneOfRuleIdx].oneOf.forEach(loader => {
         if (loader.test && loader.test.test && (loader.test.test("test.module.css") || loader.test.test("test.module.scss"))) {
