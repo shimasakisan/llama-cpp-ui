7a62604880dac5ad0ed2cb4a398f8b80a4edd1a4
shimasakisan <shimasakisan@gmail.com>
2023-06-11 18:31:15 +0100

    Migrated server and client to OpenAI API. This is an intermediate step.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import ConversationStep from './ConversationStep';
 import Intro from './Intro';
 import AutoScrollWrapper from './AutoScrollWrapper';
 
-const endPoint = 'http://localhost:8080';
+const apiUrl = 'http://localhost:8080';
 const defaultConversation = { title: 'Empty chat', steps: [] };
 
 function App() {
@@ -18,45 +18,78 @@ function App() {
   const [conversation, setConversation] = useState(defaultConversation);
   const [waiting, setWaiting] = useState(true);
 
+  function getChatCompletionRequest(userPrompt) {
+    const payload = {
+      "model": "gpt-3.5-turbo",
+      "messages": [{"role": "user", "content": userPrompt}],
+      "stream": true
+    }
+    return JSON.stringify(payload)
+  }
+ 
   function onResponseFinished(question, answer) {
     setConversation(prev => ({
       ...prev,
-      steps: [ ...prev.steps, {question, answer}]
+      steps: [...prev.steps, { question, answer }]
     }));
     setAnswer("");
     setQuestion("");
   }
 
-  function onResponseUpdate(previousChunk, newChunk) {
+  function parseServerSentEvents(data) {
+    return data.split('\n').filter(line => line.startsWith("data:")).map(line => line.substring(5));
+  }
+
+  function onResponseUpdate(previous, newChunk) {
+    let result = "";
     setWaiting(false);
-    return previousChunk + newChunk;
+    try {
+      const events = parseServerSentEvents(newChunk);
+      if (events) {
+        events.forEach(ev => {
+          const parsed = JSON.parse(ev);
+          const c0 = parsed && parsed.choices && parsed.choices[0];
+          if (!c0) return;
+          const eventText = c0 && c0.delta && c0.delta.content;
+          result += eventText;
+        });
+      }
+      setAnswer(previous + result);
+    } catch {
+      console.log("Error parsing response")
+    }
+    return result;
   }
 
   async function handleSubmit() {
     setIsBusy(true);
     setQuestion(input);
-    setAnswer("...");
+    setAnswer("");
     setWaiting(true);
     setInput("");
-    let tmpAnswer = "";
-    const response = await fetch(endPoint + '/chat', { method: "POST", body: input });
-    const reader = response.body.getReader();
-    while (true) {
-      const { done, value } = await reader.read();
-      if (done) {
-        onResponseFinished(input, tmpAnswer);
-        break;
+    const payload = getChatCompletionRequest(input);
+    try {
+      const response = await fetch(apiUrl + '/v1/chat/completions', { method: "POST", body: payload });
+      const reader = response.body.getReader();
+      let partialResponse = "";
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) {
+          onResponseFinished(input, partialResponse);
+          break;
+        }
+        const newChunk = new TextDecoder().decode(value);
+        partialResponse += onResponseUpdate(partialResponse, newChunk);
       }
-      const newChunk = new TextDecoder().decode(value);
-      tmpAnswer = onResponseUpdate(tmpAnswer, newChunk);
-      setAnswer(tmpAnswer);
+    } catch (err) {
+      console.log("Error fetching:", err);
     }
     setIsBusy(false);
   }
 
   async function handleCancel() {
     // TODO: Check response
-    await fetch(endPoint + '/cancel');
+    await fetch(apiUrl + '/cancel');
     setIsBusy(false);
   }
 
@@ -67,10 +100,10 @@ function App() {
         <div className='Container'>
           <Intro />
           <Conversation conversation={conversation} />
-          {isBusy && <ConversationStep question={question} answer={answer} waiting={waiting}/>}
-          {isBusy && <button onClick={handleCancel} className="Button">Cancel generation</button> }
+          {isBusy && <ConversationStep question={question} answer={answer} waiting={waiting} />}
+          {isBusy && <button onClick={handleCancel} className="Button">Cancel generation</button>}
           <InputBox value={input} onChange={e => setInput(e.target.value)} onSubmit={handleSubmit} disabled={isBusy} />
-          
+
         </div>
       </div>
     </AutoScrollWrapper>
--- server/webapi.cpp
+++ server/webapi.cpp
@@ -1,4 +1,5 @@
 #include "third-party/cpp-httplib/httplib.h"
+#include "third-party/json/single_include/nlohmann/json.hpp"
 #include "llamalib.h"
 #include "webutils.h"
 #include <iostream>
@@ -8,6 +9,9 @@
 #include <common.h>
 #include "webapi.h"
 #include "../frontend/build/index_html.hpp"
+
+
+using json = nlohmann::json;
 
 LlamaSession* currentSession = NULL;
 bool generation_in_progress = false;
@@ -20,81 +24,207 @@ void set_cors_headers(httplib::Response& res)
     res.set_header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 }
 
-void handle_chat(const httplib::Request& req, httplib::Response& res) {
+//void handle_chat(const httplib::Request& req, httplib::Response& res) {
+//    set_cors_headers(res);
+//
+//    if (generation_in_progress) {
+//        res.status = 403;
+//        res.body = "Generation already in progress, discarding.";
+//        fprintf(stderr, "\n[!] Generation in progress, discarded prompt: %s\n", req.body.c_str());
+//        return;
+//    }
+//
+//    // Stream response
+//    res.set_chunked_content_provider(
+//        "text/plain",
+//        [req](size_t offset, httplib::DataSink& sink) {
+//            generation_in_progress = true;
+//
+//            auto prompt = req.body;
+//            fprintf(stderr, "[+] Processing prompt: '%s'\n", prompt.c_str());
+//            currentSession->process_prompt(prompt);
+//
+//            fprintf(stderr, "[+] Generating response:\n");
+//            const char* token;
+//            while (true) {
+//                if (cancel_generation_requested) {
+//                    cancel_generation_requested = false;
+//                    fprintf(stderr, "\n[!] Generation cancelled\n");
+//                    sink.write("[Generation cancelled]", 23);
+//                    break;
+//                }
+//                token = currentSession->predict_next_token();
+//                if (token == NULL) {
+//                    break;
+//                }
+//
+//                sink.write(token, strlen(token));
+//            }
+//
+//            sink.done();
+//
+//            generation_in_progress = false;
+//            return true;
+//        }
+//    );
+//}
+//
+//void handle_test(const httplib::Request& req, httplib::Response& res) {
+//    
+//    set_cors_headers(res);
+//
+//    // Stream response
+//    res.set_chunked_content_provider(
+//        "text/plain",
+//        [req](size_t offset, httplib::DataSink& sink) {
+//            if (generation_in_progress) return true;
+//            generation_in_progress = true;
+//
+//            for (int i = 1; i < 200; i++) {
+//                if (cancel_generation_requested) {
+//                    cancel_generation_requested = false;
+//                    fprintf(stderr, "\n[!] Generation cancelled\n");
+//                    sink.write("[Generation cancelled]", 23);
+//                    break;
+//                }
+//                std::string b("Base ");
+//                sink.write(b.c_str(), b.size());
+//                std::this_thread::sleep_for(std::chrono::milliseconds(30));
+//            }
+//
+//            sink.done();
+//
+//            generation_in_progress = false;
+//            return true;
+//        }
+//    );
+//}
+
+
+void handle_completion_json(const httplib::Request& req, httplib::Response& res) {
+
+}
+
+void handle_models_json(const httplib::Request& req, httplib::Response& res) {
     set_cors_headers(res);
 
-    if (generation_in_progress) {
-        res.status = 403;
-        res.body = "Generation already in progress, discarding.";
-        fprintf(stderr, "\n[!] Generation in progress, discarded prompt: %s\n", req.body.c_str());
-        return;
+    // OpenAI models are required for some frontends to work.
+    json response = {
+        { "data", json::array({
+            {
+                { "id", "gpt-35-turbo" },
+                { "object", "model" },
+                { "owned_by", "organization_owner" },
+                //{ "permission", "model_1" },
+            },
+            {
+                { "id", "gpt-4" },
+                { "object", "model" },
+                { "owned_by", "organization_owner" },
+                //{ "permission", "model_1" },
+            },
+        })}
+    };
+
+    auto r = response.dump(2);
+
+    res.set_content(r, "application/json");
+
+    /*
+    Response:
+    {
+      "data": [
+        {
+          "id": "model-id-0",
+          "object": "model",
+          "owned_by": "organization-owner",
+          "permission": [...]
+        },
+        {
+          "id": "model-id-1",
+          "object": "model",
+          "owned_by": "organization-owner",
+          "permission": [...]
+        },
+        {
+          "id": "model-id-2",
+          "object": "model",
+          "owned_by": "openai",
+          "permission": [...]
+        },
+      ],
+      "object": "list"
+    }
+    */
+}
+
+void handle_chat_json(const httplib::Request& req, httplib::Response& res) {
+    json j = json::parse(req.body);
+
+    std::cout << "[+] REQUEST\n";
+
+    auto temperature = j.contains("temperature") ? j["temperature"].get<float>() : 0.2;
+    auto stream = j.contains("stream") ? j["stream"].get<bool>() : false;
+    if (j.contains("messages")) {
+        auto messages = j["messages"];
+        if (messages.is_array()) {
+            for (auto& msg : messages) {
+                auto role = msg["role"].get<std::string>();
+                auto content = msg["content"].get<std::string>();
+                std::cout << "   " << role << ": " << content << "\n";
+            }
+        }
     }
 
-    // Stream response
-    res.set_chunked_content_provider(
-        "text/plain",
-        [req](size_t offset, httplib::DataSink& sink) {
-            generation_in_progress = true;
-
-            auto prompt = req.body;
-            fprintf(stderr, "[+] Processing prompt: '%s'\n", prompt.c_str());
-            currentSession->process_prompt(prompt);
-
-            fprintf(stderr, "[+] Generating response:\n");
-            const char* token;
-            while (true) {
-                if (cancel_generation_requested) {
-                    cancel_generation_requested = false;
-                    fprintf(stderr, "\n[!] Generation cancelled\n");
-                    sink.write("[Generation cancelled]", 23);
-                    break;
-                }
-                token = currentSession->predict_next_token();
-                if (token == NULL) {
-                    break;
-                }
-
-                sink.write(token, strlen(token));
-            }
-
-            sink.done();
-
-            generation_in_progress = false;
-            return true;
-        }
-    );
-}
-
-void handle_test(const httplib::Request& req, httplib::Response& res) {
-    
+    /* Event stream format: 
+    https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#event_stream_format
+    */
     set_cors_headers(res);
 
-    // Stream response
-    res.set_chunked_content_provider(
-        "text/plain",
-        [req](size_t offset, httplib::DataSink& sink) {
-            if (generation_in_progress) return true;
-            generation_in_progress = true;
-
-            for (int i = 1; i < 200; i++) {
-                if (cancel_generation_requested) {
-                    cancel_generation_requested = false;
-                    fprintf(stderr, "\n[!] Generation cancelled\n");
-                    sink.write("[Generation cancelled]", 23);
-                    break;
+    if (stream) {
+        json response = {
+            {"choices", json::array({
+                {
+                    //{"finish_reason", "length"}
+                    {"delta",
+                        {
+                            {"content", "hey "}
+                        }
+                    }
                 }
-                std::string b("Base ");
-                sink.write(b.c_str(), b.size());
-                std::this_thread::sleep_for(std::chrono::milliseconds(30));
-            }
+            })}
+        };
 
-            sink.done();
+        int i = 0;
 
-            generation_in_progress = false;
-            return true;
-        }
-    );
+        res.set_chunked_content_provider("text/event-stream",
+            [i, response](size_t offset, httplib::DataSink& sink) mutable {
+                // Evaluate the prompt and stream the result
+                bool continues = true;
+                auto& choice = response["choices"][0];
+                choice["delta"]["content"] = "Sample outputs " + std::to_string(i+1) + " ";
+                if (++i == 10) {
+                    choice["finish_reason"] = "stop";
+                    continues = false;
+                }
+
+                auto payload = "data:" + response.dump() + "\n\n";
+                std::cerr << "send event:\n" << payload;
+                sink.write(payload.c_str(), payload.size());
+
+                if (!continues) sink.done();
+
+                std::this_thread::sleep_for(std::chrono::milliseconds(300));
+                
+                return continues;
+            });
+    }
+    else {
+        // The non-streaming thingy
+        //res.set_content(response.dump(2), "application/json");
+    }
 }
+
 
 void handle_cancel(const httplib::Request& req, httplib::Response& res) {
     set_cors_headers(res);
@@ -125,10 +255,13 @@ void setup_http_server(httplib::Server& svr, webapi_params& webparams) {
         });
 
     // Endpoints
-    svr.Post("/chat",   handle_chat);
-    svr.Post("/test",   handle_test);
+    //svr.Post("/chat",   handle_chat);
+    //svr.Post("/test",   handle_test);
     svr.Get("/status",  handle_status);
     svr.Get("/cancel",  handle_cancel);
+    svr.Get("/v1/completions", handle_completion_json);
+    svr.Post("/v1/chat/completions", handle_chat_json);
+    svr.Get("/v1/models", handle_models_json);
     svr.Get("/", handle_index_html);
 }
 
@@ -142,7 +275,7 @@ int main(int argc, char** argv) {
 
     LlamaSession session(&params);
 
-    int res = session.load_model();
+    /*int res = session.load_model();
     if (res) {
         fprintf(stderr, "[!] Load model failed");
         return 12;
@@ -160,7 +293,7 @@ int main(int argc, char** argv) {
     }
 
     fprintf(stderr, "\n[+] Initial prompt processed. Ready to accept requests.\n");
-    currentSession = &session;
+    currentSession = &session;*/
 
     session.m_prompt_prefix = webparams.prompt_prefix;
     session.m_prompt_suffix = webparams.prompt_suffix;
